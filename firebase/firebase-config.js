5d2531aa08b6402e5817aa18fbd25073223b8abc
Mohan Perera <kmohan.perera@gmail.com>
2026-09-19 00:15:46 +1000

    Release tested Accounts module to production

---
+++ firebase/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  "apiKey": "AIzaSyCEgrfFn0zEw2dDwvPuTsYsT61p_SbGehs",
+  "authDomain": "ja-ela-serenity-villa.firebaseapp.com",
+  "projectId": "ja-ela-serenity-villa",
+  "storageBucket": "ja-ela-serenity-villa.firebasestorage.app",
+  "messagingSenderId": "353301654839",
+  "appId": "1:353301654839:web:01fb5fc78ac1ced5fabeb"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
